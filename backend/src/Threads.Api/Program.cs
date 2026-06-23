abe1e2dfc8c5694a4c2aaf369b5ca0f806ecaa61
Fall1ngLight <keruloszabolcs1@gmail.com>
2026-06-23 19:30:08 +0000

    fix: stopped catching HostAbortedException thrown by ef migrations

--- backend/src/Threads.Api/Program.cs
+++ backend/src/Threads.Api/Program.cs
@@ -9,12 +9,12 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
     builder.AddServices();
-
+    
     var app = builder.Build();
     app.Configure();
     app.Run();
 }
-catch (Exception ex)
+catch (Exception ex) when(ex is not HostAbortedException)
 {
     Log.Fatal(ex, "Application terminated unexpectedly");
 }
